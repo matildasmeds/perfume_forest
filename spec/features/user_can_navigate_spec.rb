176539a6914ed88330543e3b0a3a3e067bbc39de
matildasmeds <matilda.smeds@gmail.com>
2016-04-09 18:11:12 +0300

    Add navigation specs and refactor

--- spec/features/user_can_navigate_spec.rb
+++ spec/features/user_can_navigate_spec.rb
@@ -1,28 +1,54 @@
 require 'spec_helper'
 require_relative 'helpers/user_can_navigate_spec_helper'
-# Note: Capybara with selenium is not able
-# to navigate these. Reason unknown.
 RSpec.feature 'User can navigate between views' do
 
   before(:all) { setup_note_and_perfume }
 
-  scenario 'perfumes -> perfume' do
-    start_at perfumes_path
-    assert_navigates_to perfume, :show
+  describe 'To perfumes # show' do
+    scenario 'from perfumes # index' do
+      start_at perfumes_path
+      assert_navigates_to perfume, :show
+    end
+
+    scenario 'from notes # show' do
+      start_at note_path(note.id)
+      assert_navigates_to perfume, :show
+    end
   end
 
-  scenario 'note -> perfume' do
-    start_at note_path(note.id)
-    assert_navigates_to perfume, :show
+  describe 'To notes # show' do
+    scenario 'from perfumes # show' do
+      start_at perfume_path(perfume.id)
+      assert_navigates_to note, :show
+    end
+
+    scenario 'from notes # index' do
+      start_at notes_path
+      assert_navigates_to note, :show
+    end
   end
 
-  scenario 'perfume -> note' do
-    start_at perfume_path(perfume.id)
-    assert_navigates_to note, :show
+  describe 'To notes # index' do
+    scenario 'from notes # show' do
+      start_at note_path(note.id)
+      assert_navigates_to_notes
+    end
+
+    scenario 'from perfumes # show' do
+      start_at perfume_path(perfume.id)
+      assert_navigates_to_perfumes
+    end
   end
 
-  scenario 'notes -> note' do
-    start_at notes_path
-    assert_navigates_to note, :show
+  describe 'To perfumes # index' do
+    scenario 'from notes # show' do
+      start_at note_path(note.id)
+      assert_navigates_to_perfumes
+    end
+
+    scenario 'from perfumes # show' do
+      start_at perfume_path(perfume.id)
+      assert_navigates_to_perfumes
+    end
   end
 end
