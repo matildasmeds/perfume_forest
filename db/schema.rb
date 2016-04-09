f8ce32e896b738a5c3e444fc0c3399f6f1a07d40
matildasmeds <matilda.smeds@gmail.com>
2016-04-09 19:38:03 +0300

    Add database level constraints and new indexes

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403093744) do
+ActiveRecord::Schema.define(version: 20160409152811) do
 
   create_table "brands", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
+    t.string   "name",       null: false
     t.integer  "year"
   end
+
+  add_index "brands", ["name"], name: "index_brands_on_name", unique: true
 
   create_table "layer_types", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
+    t.string   "name",       null: false
   end
 
   create_table "notes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
+    t.string   "name",       null: false
   end
+
+  add_index "notes", ["name"], name: "index_notes_on_name", unique: true
 
   create_table "perfume_notes", force: :cascade do |t|
     t.datetime "created_at",    null: false
@@ -40,15 +44,16 @@ ActiveRecord::Schema.define(version: 20160403093744) do
     t.integer  "layer_type_id"
   end
 
-  add_index "perfume_notes", ["note_id"], name: "index_perfume_notes_on_note_id"
-  add_index "perfume_notes", ["perfume_id"], name: "index_perfume_notes_on_perfume_id"
+  add_index "perfume_notes", ["perfume_id", "note_id", "layer_type_id"], name: "index_perfume_notes_constraint", unique: true
 
   create_table "perfumes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "year"
-    t.string   "name"
-    t.integer  "brand_id"
+    t.string   "name",       null: false
+    t.integer  "brand_id",   null: false
   end
+
+  add_index "perfumes", ["name", "brand_id"], name: "index_perfumes_on_name_and_brand_id", unique: true
 
 end
